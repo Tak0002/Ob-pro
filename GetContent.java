552358e2b5a8fbb92eee3a940ebb3334cca992cb
Satosi Sakai <s12369ss@gmail.com>
2015-01-06 20:17:56 +0900

    URLからデータ取得

--- GetContent.java
+++ GetContent.java
@@ -1,13 +1,54 @@
-package jp.ac.keio.sfc.sakai;
+package jp.ac.keio.sfc.obpro;
 
 import java.io.*;
 import java.net.*;
+import com.fasterxml.jackson.core.*;
 
 public class GetContent {
 
     public static void main (String args[]) {
         try {
             URL url = new URL("http://wepia.biz/wellness/wellness_api.php");
+            JsonFactory factory = new JsonFactory();
+        	JsonParser parser = factory.createParser(url);
+        	// 配列の処理
+    		if (parser.nextToken() == JsonToken.START_ARRAY) {
+    			while (parser.nextToken() != JsonToken.END_ARRAY) {
+    				// 各オブジェクトの処理
+    				if (parser.getCurrentToken() == JsonToken.START_OBJECT) {
+    					while (parser.nextToken() != JsonToken.END_OBJECT) {
+    						String name = parser.getCurrentName();
+    						parser.nextToken();
+    						// "id"フィールド
+    						if ("id".equals(name)) {
+    							System.out.println(name + ": ");
+    							while (parser.nextToken() != JsonToken.END_OBJECT) {
+    								if (parser.getCurrentToken() == JsonToken.FIELD_NAME) {
+    									System.out.print("    " + parser.getText() + ": ");
+    								} else if (parser.getCurrentToken() == JsonToken.VALUE_STRING) {
+    									System.out.println(parser.getText());
+    								}
+    							}
+    						}
+    						// "message"フィールド
+    						else if ("message".equals(name)) {
+    							System.out.println(name + ": " + parser.getText());
+    						} 
+    						else {
+    							parser.skipChildren();
+    						}
+    					}
+    				}
+    				else {
+    					parser.skipChildren();
+    				}        
+    			}
+    		}
+    		else {
+    			parser.skipChildren();
+    		}
+        	
+    		/*
             Object content = url.getContent();
             if (content instanceof InputStream) {
                 BufferedReader bf = new BufferedReader(new InputStreamReader( (InputStream)content) );        
@@ -19,6 +60,7 @@ public class GetContent {
             else {
                 System.out.println(content.toString());	
             }
+            */
         }
         catch (ArrayIndexOutOfBoundsException e) {
             System.err.println("引数にURLを指定してください");
